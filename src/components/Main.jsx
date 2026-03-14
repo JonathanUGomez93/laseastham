9eea136f89f87c1fa8d2070f11352a67b71bff26
Fernando Gomez <fegomez@MacBook-Pro-de-Fernando.local>
2026-03-14 19:03:44 -0300

    agrego imagenes y hover en main

--- src/components/Main.jsx
+++ src/components/Main.jsx
@@ -1,19 +1,29 @@
-import { Link } from "react-router"
-import '../css/main.css'
-const Main =()=>{
-    return(
-    <>
-    <div className="members">
-        <Link to='/alayna'>ALAYNA</Link>
-        <Link to='/bruna'>BRUNA</Link>
-        <Link to='/joline'>JOLINE</Link>
-        <Link to='/aitana'>AITANA</Link>
-        <Link to='/haruko'>HARUKO</Link>
-        <Link to='/giovanna'>GIOVANNA</Link>
-        <Link to='/evangelina'>EVANGELINA</Link>
-    </div>
-    </>
-    )
-}
+import { Link } from "react-router";
+import '../css/main.css';
 
-export default Main
+const Main = () => {
+  const setBodyBackground = (url) => {
+    document.body.style.backgroundImage = `url(${url})`;
+    document.body.style.backgroundSize = "cover";
+    document.body.style.backgroundPosition = "center";
+    document.body.style.transition = "background-image 1s ease-in-out";
+  };
+  const resetBodyBackground = () => {
+    document.body.style.backgroundImage = "";
+    document.body.style.transition = "background-image 1s ease-in-out";
+  };
+
+  return (
+    <div className="members">
+      <Link to="/alayna" onMouseEnter={() => setBodyBackground("/images/alayna.jpg")} onMouseLeave={resetBodyBackground}>ALAYNA</Link>
+      <Link to="/bruna" onMouseEnter={() => setBodyBackground('/images/bruna.jpg')} onMouseLeave={resetBodyBackground}>BRUNA</Link>
+      <Link to="/joline" onMouseEnter={() => setBodyBackground('/images/joline.jpg')} onMouseLeave={resetBodyBackground}>JOLINE</Link>
+      <Link to="/aitana" onMouseEnter={() => setBodyBackground('/images/aitana.jpg')}onMouseLeave={resetBodyBackground}>AITANA</Link>
+      <Link to="/haruko" onMouseEnter={() => setBodyBackground('/images/haruko.jpg')} onMouseLeave={resetBodyBackground}>HARUKO</Link>
+      <Link to="/giovanna" onMouseEnter={() => setBodyBackground('/images/giovanna.jpg')} onMouseLeave={resetBodyBackground}>GIOVANNA</Link>
+      <Link to="/evangelina" onMouseEnter={() => setBodyBackground('/images/evangelina.jpg')} onMouseLeave={resetBodyBackground}>EVANGELINA</Link>
+    </div>
+  );
+};
+
+export default Main;
--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -4,10 +4,10 @@ import '../css/navbar.css'
 const Navbar =()=>{
     return(
         <div className='routes'>
+            <Link to='Nosotras'>Nosotras</Link>
             <Link to='/CANCELED'>Get CANCELED</Link>
             <Link to='Events'>Events</Link>
             <Link to='Socios'>Socios</Link>
-            <Link to='Nosotras'>Nosotras</Link>
         </div>
     )
 }
