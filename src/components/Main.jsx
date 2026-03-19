1e5df04b8767f9dfa44dfff815e3d0c446e05fc5
Fernando Gomez <fegomez@MacBook-Pro-de-Fernando.local>
2026-03-19 19:02:20 -0300

    removed dynamic buggy background image for a background video

--- src/components/Main.jsx
+++ src/components/Main.jsx
@@ -2,28 +2,20 @@ import { Link } from "react-router";
 import '../css/main.css';
 
 const Main = () => {
-  const setBodyBackground = (url) => {
-    document.body.style.backgroundImage = `url(${url})`;
-    document.body.style.backgroundSize = "cover";
-    document.body.style.backgroundPosition = "center";
-    document.body.style.transition = "background-image 1s ease-in-out";
-  };
-  const resetBodyBackground = () => {
-    document.body.style.backgroundImage = "";
-    document.body.style.transition = "background-image 1s ease-in-out";
-  };
-
   return (
-    <div className="members">
-      <Link to="/alayna" onMouseEnter={() => setBodyBackground("/images/alayna.jpg")} onMouseLeave={resetBodyBackground}>ALAYNA</Link>
-      <Link to="/bruna" onMouseEnter={() => setBodyBackground('/images/bruna.jpg')} onMouseLeave={resetBodyBackground}>BRUNA</Link>
-      <Link to="/joline" onMouseEnter={() => setBodyBackground('/images/joline.jpg')} onMouseLeave={resetBodyBackground}>JOLINE</Link>
-      <Link to="/aitana" onMouseEnter={() => setBodyBackground('/images/aitana.jpg')}onMouseLeave={resetBodyBackground}>AITANA</Link>
-      <Link to="/haruko" onMouseEnter={() => setBodyBackground('/images/haruko.jpg')} onMouseLeave={resetBodyBackground}>HARUKO</Link>
-      <Link to="/giovanna" onMouseEnter={() => setBodyBackground('/images/giovanna.jpg')} onMouseLeave={resetBodyBackground}>GIOVANNA</Link>
-      <Link to="/evangelina" onMouseEnter={() => setBodyBackground('/images/evangelina.jpg')} onMouseLeave={resetBodyBackground}>EVANGELINA</Link>
+    <div className="bgContainer">
+      <video autoPlay loop muted playsInline className="video-bg">
+        <source src="https://assets.codepen.io/6093409/river.mp4" type="video/mp4" /></video>
+      <div className="members">
+        <Link to="/alayna">ALAYNA</Link>
+        <Link to="/bruna">BRUNA</Link>
+        <Link to="/joline">JOLINE</Link>
+        <Link to="/aitana">AITANA</Link>
+        <Link to="/haruko">HARUKO</Link>
+        <Link to="/giovanna">GIOVANNA</Link>
+        <Link to="/evangelina">EVANGELINA</Link>
+      </div>
     </div>
   );
 };
-
 export default Main;
